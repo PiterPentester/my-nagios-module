2b75bcf362e82b5363580126d6d0f36f7c6df0d2
Bohdan Pakhomov <shtormless@gmail.com>
2020-05-27 12:44:53 +0300

    module: use nrpe.cfg template

--- nagios/manifests/config_agent.pp
+++ nagios/manifests/config_agent.pp
@@ -8,6 +8,14 @@ class nagios::config_agent inherits nagios {
       content => template('nagios/nrpe.cfg.erb'),
     }
 
+    file { '/usr/local/nagios/etc/nrpe.cfg':
+      ensure  => file,
+      owner   => 'nagios',
+      group   => 'nagios',
+      mode    => '0644',
+      content => template('nagios/nrpe1.cfg.erb'),
+    }
+
     exec { "sed -i '/^allowed_hosts=/s/$/,10.25.5.2/' /usr/local/nagios/etc/nrpe.cfg":
       path => '/usr/bin:/usr/sbin:/bin',
       user => 'root',
